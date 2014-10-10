eace7134e3b073043e34811c283038cc03ca8c15
Swathi Kotturu <kotturuswathi@yahoo.com>
2014-10-09 23:29:26 -0700

    Added Activities
    Game now works, but still have to do orientation log reports, and
    orientation layout changes

---
+++ src/cs175/skotturu/dizphone/GameOverActivity.java
@@ -0,0 +1,14 @@
+package cs175.skotturu.dizphone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameOverActivity extends Activity {
+	
+	@Override 
+	public void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState); 
+		setContentView(R.layout.game_over);
+	}
+
+}
---
+++ src/cs175/skotturu/dizphone/MainActivity.java
@@ -0,0 +1,86 @@
+package cs175.skotturu.dizphone;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+	Button startgbutton;
+    Button settingsbutton;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnStartGameButton();
+        addListenerOnSettingsButton();
+    }
+    
+	public void addListenerOnStartGameButton() {
+
+		final Context context = this;
+
+		startgbutton = (Button) findViewById(R.id.start_game_button);
+
+		startgbutton.setOnClickListener(new OnClickListener() {
+
+			//@Override
+			public void onClick(View arg0) {
+
+				Intent intent = new Intent(context, StartGameActivity.class);
+				startActivity(intent);
+
+			}
+
+		});
+
+	}
+	
+	public void addListenerOnSettingsButton() {
+
+		final Context context = this;
+
+		settingsbutton = (Button) findViewById(R.id.settings_button);
+
+		settingsbutton.setOnClickListener(new OnClickListener() {
+
+			//@Override
+			public void onClick(View arg0) {
+
+				Intent intent = new Intent(context, SettingsActivity.class);
+				startActivity(intent);
+
+			}
+
+		});
+
+	}
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
